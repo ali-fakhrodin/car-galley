f48b8e7fad66cacd2e3169f6f7ada2e61fb09868
AliFakhrodin <alifakhrodin16@gmail.com>
2024-07-31 16:40:50 +0330

    make swiper slides

---
+++ js/script-swiper.js
@@ -0,0 +1,39 @@
+
+const swiper = new Swiper('.swiper', {
+     slidesPerView: 4,
+     spaceBetween: -10,
+     autoplay: {
+          delay: 2000
+     },
+     direction: getDirection(),
+     grabCursor: true,
+     scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
+     },
+     // navigation: {
+     //      nextEl: '.swiper-button-next',
+     //      prevEl: '.swiper-button-prev',
+     // },
+     pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+     },
+     keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+     },
+     on: {
+          resize: function () {
+               swiper.changeDirection(getDirection());
+          },
+     },
+
+});
+
+function getDirection() {
+     var windowWidth = window.innerWidth;
+     var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+     return direction;
+}
